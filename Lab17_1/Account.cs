9e7dd5042deafddea3a93ae8c48c6918d6800368
milov <milov@DESKTOP-M98P6EU>
2022-01-02 21:11:23 +0300

    Добавьте файлы проекта.

---
+++ Lab17_1/Account.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab17_1
+{
+    class Account<T>
+    {
+        public T Num { get; set; }
+        public string Name { get; set; }
+        public double Balance { get; set; }
+        public string GetInfo()
+        {
+            return $"{Num} {Name} {Balance}";
+        }
+        public void Input()
+        {
+            Console.WriteLine("Введите номер счета");
+            string stringNum = Console.ReadLine();
+            Num = (T)Convert.ChangeType(stringNum, typeof(T));
+
+            Console.WriteLine("Введите ФИО");
+            Name = Console.ReadLine();
+            Console.WriteLine("Введите баланс");
+            Balance = Convert.ToDouble(Console.ReadLine());
+        }
+    }
+}
---
+++ Lab17_1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab17_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Account<int> account1 = new Account<int>();
+            account1.Input();
+            Console.WriteLine(account1.GetInfo());
+
+            Account<string> account2 = new Account<string>();
+            account2.Input();
+            Console.WriteLine(account2.GetInfo());
+            Console.WriteLine("Нажмите любую клавишу для завершения");
+            Console.ReadKey();
+        }
+
+    }
+}
